5a91fdc67e7b1d412233bbbf7caa83e5a630009c
Adalberto <adalberto8648@gmail.com>
2025-01-20 23:28:13 -0300

    main

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
+import tkinter as tk
 
 def criar_tabuleiro():
-    tabuleiro = [['.' for _ in range(8)] for _ in range(8)]
+    tabuleiro = [['.' for linha in range(8)] for coluna in range(8)]
     return tabuleiro
 
-def mostrar_tabuleiro(tabuleiro):
-    for linha in tabuleiro:
-        print(' '.join(linha))
+def mostrar_tabuleiro(canvas, tabuleiro):
+    tamanho_casa = 60
+    for linha in range(8): #percorre enumerando 0 a 7 , 8x
+        for coluna in range(8): #percorre enumerando as colunas 0 a 7, 8x
+            x1 = linha * tamanho_casa #percorre as 8 linhas multiplicando por 60
+            y1 = coluna * tamanho_casa #percorre
+            x2 = x1 + tamanho_casa
+            y2 = y1 + tamanho_casa
 
-tabuleiro = criar_tabuleiro()
-mostrar_tabuleiro(tabuleiro)
+            if (linha + coluna) % 2 == 0:
+                cor = 'white'
+            else:
+                cor = 'black'
+            canvas.create_rectangle(x1, y1, x2, y2, fill=cor, outline='gray')
+
+def criar_janela():
+    janela = tk.Tk()
+    janela.title("Jogo de Damas")
+
+    canvas = tk.Canvas(janela, width=480, height=480)
+    canvas.pack()
+
+    tabuleiro = criar_tabuleiro()
+    mostrar_tabuleiro(canvas, tabuleiro)
+
+    janela.mainloop()
+
+criar_janela()
+
 
---
+++ teste.py
@@ -0,0 +1,3 @@
+for i in range(8):  # Linhas
+    for j in range(8):  # Colunas
+        print(f"Posição: ({i}, {j})")
