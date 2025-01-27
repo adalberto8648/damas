c403291be6cf7692c973b278dc6761edf4fb12c4
Adalberto <adalberto8648@gmail.com>
2025-01-27 10:07:59 -0300

    peças

--- main.py
+++ main.py
@@ -4,10 +4,18 @@ import tkinter as tk
 def criar_tabuleiro():
     tabuleiro = [['.' for linha in range(8)] for coluna in range(8)]
 
+    #posiciona peças brancas 'p'
     for linha in range(3):
         for coluna in range(8):
             if (linha + coluna) % 2 == 1:
                 tabuleiro[linha][coluna] = 'p'
+    
+    #posiciona peças vermelhas 'r'
+    for linha in range(5, 8):
+        for coluna in range(8):
+            if (linha + coluna) % 2 == 1:
+                tabuleiro[linha][coluna] = 'r'
+
     return tabuleiro
 
 #exibição
@@ -15,8 +23,8 @@ def mostrar_tabuleiro(canvas, tabuleiro):
     tamanho_casa = 60
     for linha in range(8):
         for coluna in range(8):
-            x1 = linha * tamanho_casa
-            y1 = coluna * tamanho_casa
+            x1 = coluna * tamanho_casa
+            y1 = linha * tamanho_casa
             x2 = x1 + tamanho_casa
             y2 = y1 + tamanho_casa
 
@@ -25,11 +33,17 @@ def mostrar_tabuleiro(canvas, tabuleiro):
                 cor = 'white'
             else:
                 cor = 'black'
+
             #montagem da grade pelo canvas
             canvas.create_rectangle(x1, y1, x2, y2, fill=cor, outline='gray')
 
-            if tabuleiro[coluna][linha] == 'p' and cor == 'black':  # Se tiver uma peça preta
+            #desenhar as peças brancas
+            if tabuleiro[linha][coluna] == 'p':
                 canvas.create_oval(x1 + 10, y1 + 10, x2 - 10, y2 - 10, fill='white')
+
+            #desenhar as peças vermelhas
+            elif tabuleiro[linha][coluna] == 'r':
+                canvas.create_oval(x1 + 10, y1 + 10, x2 - 10, y2 - 10, fill='green')
 
 def criar_janela():
     janela = tk.Tk()
